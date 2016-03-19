605b2d1329add7f456c97eda3ef8038d54b3ed0d
Sergio11 <sss4esob@gmail.com>
2016-03-19 13:58:30 +0100

    add Mp3Source

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var Mp3Library = require('./lib/Mp3Library');
-
+var Mp3Source = require('./lib/Mp3Source.js');
 
 var library = new Mp3Library({ basedir: './songs/' });
 
-
 library.on("ready",function(){
     console.log("Mp3 ready");
+    var mp3source = new Mp3Source(library);
+    mp3source.on('track', function(){
+        
+    });
+    mp3source.on('frame', function(frame){
+        console.log("Frame ....");
+        console.log(frame);
+    });
 });
